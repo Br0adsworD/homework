a7803604fff990898b4a72949537ac92a29444c4
Konstantin <Konstantin>
2017-01-16 20:14:14 +0200

    second commit

--- index.php
+++ index.php
@@ -9,6 +9,4 @@ $to->saveRes($ans);
 $tea=$to->getAns("tea");
 $coffee=$to->getAns("coffee");
 $refrain=$to->getAns("refrain");
-echo "за чай-".$tea."<br/>";
-echo "зы кофе-".$coffee."<br/>";
-echo "воздержались-".$refrain."<br/>";
+
